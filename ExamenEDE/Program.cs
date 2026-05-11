0e2ff0a08ad69088988a48fb8255e4486d9db1aa
Alumno1 <calderongramageadrian@gmail.com>
2026-05-11 09:16:40 +0200

    "Ejercicio 1: clase Pelicula implementada"

--- ExamenEDE/Program.cs
+++ ExamenEDE/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Pelicula
+{
+
+private string titulo;
+    public string director;
+    private int anyo;
+        private bool disponible;
+
+    public Pelicula(string titulo, string director, int anyo, bool disponible)
+    {
+        this.titulo = titulo;
+        this.director = director;
+        this.anyo = anyo;
+        this.disponible = disponible;
+    }
+
+    public string getTitulo() { return titulo; }
+    public string getDirector() { return director; }
+    public int getAnyo() { return anyo; }
+    public bool isDisponible() { return disponible; }
+
+
+    public override string ToString()
+    {
+        return titulo + " - " + director + " (" + anyo + ")";
+    }
+}
